e5de60d9a76b5fd6c22274008f5aab47c5e868ad
uinika <uinika@163.com>
2016-02-18 15:28:02 +0800

    Support less

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,33 @@
-var gulp = require("gulp"),
-    livereload = require("gulp-livereload"),
-    connect = require('gulp-connect');
+const gulp = require("gulp"),
+  livereload = require("gulp-livereload"),
+  connect = require('gulp-connect');
+  less = require('gulp-less');
 
 //Static Server
 gulp.task('connect', function() {
-    connect.server({
-        root: './client/',
-        port: 5000,
-        livereload: true
-    });
+  connect.server({
+    root: './client/',
+    port: 5000,
+    livereload: true
+  });
 });
 
 //Reload Server
 gulp.task('livereload',[],function() {
-    gulp.src('./client/' + '**/*.*')
-      .pipe(connect.reload());
+  gulp.src('./client/' + '**/*.*')
+  .pipe(connect.reload());
+});
+
+// Less Processor
+gulp.task('less', function () {
+  return gulp.src('./client/styles/' + '**/*.less')
+  .pipe(less())
+  .pipe(gulp.dest('./client/styles/'));
 });
 
 // Watch
 gulp.task('watch', function() {
-    gulp.watch('./client/' + '**/*.*',['livereload']);
+  gulp.watch('./client/' + '**/*.*',['livereload', 'less']);
 })
 
 // Make defulat task
