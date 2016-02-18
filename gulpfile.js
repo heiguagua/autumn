2f66a318dd2fe8adb136749696a58d8c755660cc
uinika <uinika@163.com>
2016-02-18 14:37:39 +0800

    Support live reload

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,26 @@
-var gulp = require('gulp');
- var gls = require('gulp-live-server');
+var gulp   = require("gulp"),
+    livereload   = require("gulp-livereload"),
+    connect = require('gulp-connect');
 
-gulp.task('start', function() {
-  var server = gls.new('./server/app.js');
-  server.start();
-
+//Static Server
+gulp.task('connect', function() {
+    connect.server({
+        root: './client',
+        port: 5000,
+        livereload: true
+    });
 });
+
+//Reload Server
+gulp.task('livereload',[],function() {
+    gulp.src('./client' + '**/*.*')
+      .pipe(connect.reload());
+});
+
+// Watch
+gulp.task('watch', function() {
+    gulp.watch('./client' + '**/*.*',['livereload']);
+})
+
+// Make defulat task
+gulp.task('default', ['connect', 'watch']);
