d0003442c37f63ebe5500618cb6b69779237fdb7
uinika <uinika@163.com>
2016-02-23 19:30:44 +0800

    Add login.html into the partials

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,29 +1,11 @@
 var app = angular.module('app', ['ui.router']);
 app.config(function($stateProvider, $urlRouterProvider) {
   // For any unmatched url, redirect to /state1
-  $urlRouterProvider.otherwise("/state1");
+  $urlRouterProvider.otherwise("/login");
   // Now set up the states
   $stateProvider
-    .state('state1', {
-      url: "/state1",
-      templateUrl: "scripts/partials/state1.html"
-    })
-    .state('state1.list', {
-      url: "/list",
-      templateUrl: "scripts/partials/state1.list.html",
-      controller: function($scope) {
-        $scope.items = ["A", "List", "Of", "Items"];
-      }
-    })
-    .state('state2', {
-      url: "/state2",
-      templateUrl: "scripts/partials/state2.html"
-    })
-    .state('state2.list', {
-      url: "/list",
-      templateUrl: "scripts/partials/state2.list.html",
-      controller: function($scope) {
-        $scope.things = ["A", "Set", "Of", "Things"];
-      }
+    .state('login', {
+      url: "/login",
+      templateUrl: "scripts/partials/login.html"
     });
 });
