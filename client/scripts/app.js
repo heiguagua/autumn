1e4f2e61545224bc4119c94244d359ce05dc6637
uinika <uinika@163.com>
2016-02-24 11:05:56 +0800

    Add main.html which attached partials

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -5,13 +5,18 @@ var app = angular.module('app', ['ui.router','loginControllers']);
 
 app.config(function($stateProvider, $urlRouterProvider) {
   // For any unmatched url, redirect to /login
-  $urlRouterProvider.otherwise("/login");
+  $urlRouterProvider.otherwise('/login');
 
   // Now set up the states
   $stateProvider
     .state('login', {
-      url: "/login",
-      templateUrl: "scripts/partials/login.html",
+      url: '/login',
+      templateUrl: 'scripts/partials/login.html',
       controller: 'LoginController'
-    });
+    })
+    .state('main',{
+      url: '/main',
+      templateUrl: 'scripts/partials/main.html',
+    })
+    ;
 });
