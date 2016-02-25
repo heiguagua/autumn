2504adf570c0c9444cfd070e3244fc555e540a7e
uinika <uinika@163.com>
2016-02-25 14:31:54 +0800

    Make the http dev-server available

--- client/scripts/services/common.js
+++ client/scripts/services/common.js
@@ -3,10 +3,15 @@
 
 var commonService = angular.module('commonServices', ['ngResource']);
 
-commonService.service('LoginService', ['$resource',
-  function($resource) {
-    return $resource('login', {}, {
-      query: {method:'POST', params:{username:'admin', password: 'password'}, isArray:true}
+commonService.service('LoginService', ['$resource', '$http',
+  function($resource, $http) {
+    $http({
+      method: 'POST',
+      url: 'http://localhost:5000/api/user'
+    }).success(function(data, status, headers, config) {
+      console.log(data);
+    }).error(function(data, status, headers, config) {
+      console.error("HTTP Status Code : "+status);
     });
   }
 ]);
