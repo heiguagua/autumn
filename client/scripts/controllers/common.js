d817bafd18a8d5be6933bebe051c3ab4dd3af137
xulan <xulan@chinawiserv.com>
2016-02-29 18:07:34 +0800

    Modify navbar menu not using slideout.js ; Fullfill dashboard content

--- client/scripts/controllers/common.js
+++ client/scripts/controllers/common.js
@@ -19,10 +19,7 @@ commonControllers.controller('LoginController', ['$scope', '$state', 'LoginServi
 
 commonControllers.controller('MainController',['$scope',
   function($scope){
-    $scope.modalShown = false;
-    $scope.toggleModal = function() {
-      $scope.modalShown = !$scope.modalShown;
-    };
+    
   }
 ])
 
--- client/scripts/directives/common.js
+++ client/scripts/directives/common.js
@@ -18,16 +18,44 @@ commonDirectives.directive("mainWrapper",[function() {
     restrict : "AE",
     replace: true,
     link: function(scope, element, attrs){
-      var slideout = new Slideout({
-        'panel': element.find('#content')[0],
-        'menu': element.find('#navMenu')[0],
-        'padding': 250,
-        'tolerance': 70
-      });
-      slideout.open();
+
       element.find('#sidebarToggler')[0].addEventListener('click', function() {
-        slideout.toggle();
+        var $content = $(element.find('#content')[0]);
+        var $navMenu = $(element.find('#navMenu')[0]);
+        $content.toggleClass("content-collapse");
+        $navMenu.toggleClass("ml-250");
       });
+    }
+  }
+}]);
+
+/*content view directive*/
+commonDirectives.directive("contentView",[function() {
+  return {
+    restrict : "AE",
+    replace : true,
+    link : function(scope, element, attrs) {
+      $('#table').bootstrapTable({
+        columns: [{
+            field: 'id',
+            title: 'Item ID'
+        }, {
+            field: 'name',
+            title: 'Item Name'
+        }, {
+            field: 'price',
+            title: 'Item Price'
+        }],
+        data: [{
+            id: 1,
+            name: 'Item 1',
+            price: '$1'
+        }, {
+            id: 2,
+            name: 'Item 2',
+            price: '$2'
+        }]
+    });
     }
   }
 }]);
