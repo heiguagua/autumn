144e6e94f7865b40e44a3576a2aac068ba62998f
xulan <xulan@chinawiserv.com>
2016-03-02 16:48:46 +0800

    Add Data Resource Module

---
+++ client/scripts/directives/dataResource.js
@@ -0,0 +1,98 @@
+'use strict';
+/* Data Resource Directives */
+
+var DataResourceDirective = angular.module('DataResourceDirective', ['DataResourceService']);
+
+DataResourceDirective.directive("wiservDataResource", [
+  function() {
+    return {
+      restrict: "AE",
+      replace: true,
+      link: function(scope, element, attrs) {
+        element.find('.selectpicker').selectpicker({
+          style: 'btn-default btn-sm',
+          width: 80,
+          liveSearch: false
+        });
+
+
+        //var date = new Date();
+        //var startDate = moment(date).subtract(30, 'days').startOf('days');
+        //var endDate = moment(date).endOf('days');
+        //element.find('#createTime_query').daterangepicker({'startDate': '2016-02-01 00:00:00', 'endDate': '2016-03-01 00:00:00'}, //null, date);
+
+        element.find('#table').bootstrapTable({
+          columns: [{
+            field: 'state',
+            checkbox: true
+          }, {
+            field: 'resName',
+            title: '资源名称'
+          }, {
+            field: 'resCat',
+            title: '资源源目录分类'
+          }, {
+            field: 'resDept',
+            title: '资源所属部门'
+          }, {
+            field: 'resCreateTime',
+            title: '资源创建时间'
+          }, {
+            field: 'resAccessStaus',
+            title: '资源接入状态'
+          }],
+          data: [{
+            resName: '交通数据',
+            resCat: '服务一',
+            resDept: '交通局',
+            resCreateTime:'2016-02-10 10:15:21',
+            resAccessStaus:'未接入'
+          }, {
+            resName: '交通数据',
+            resCat: '服务一',
+            resDept: '交通局',
+            resCreateTime:'2016-02-10 10:15:21',
+            resAccessStaus:'未接入'
+          }, {
+            resName: '交通数据',
+            resCat: '服务一',
+            resDept: '交通局',
+            resCreateTime:'2016-02-10 10:15:21',
+            resAccessStaus:'未接入'
+          }, {
+            resName: '交通数据',
+            resCat: '服务一',
+            resDept: '交通局',
+            resCreateTime:'2016-02-10 10:15:21',
+            resAccessStaus:'未接入'
+          }, {
+            resName: '交通数据',
+            resCat: '服务一',
+            resDept: '交通局',
+            resCreateTime:'2016-02-10 10:15:21',
+            resAccessStaus:'未接入'
+          }, {
+            resName: '交通数据',
+            resCat: '服务一',
+            resDept: '交通局',
+            resCreateTime:'2016-02-10 10:15:21',
+            resAccessStaus:'未接入'
+          }, {
+            resName: '交通数据',
+            resCat: '服务一',
+            resDept: '交通局',
+            resCreateTime:'2016-02-10 10:15:21',
+            resAccessStaus:'未接入'
+          }],
+          pagination: true,
+          pageNumber: 1,
+          toolbar: ".toolbar",
+          clickToSelect: true,
+          showRefresh: true,
+          showToggle: true,
+          showColumns: true
+        });
+      }
+    }
+  }
+]);
---
+++ client/scripts/services/dataResource.js
@@ -0,0 +1,10 @@
+'use strict';
+/* Data Resource Service */
+
+var DataResourceService = angular.module('DataResourceService', []);
+
+DataResourceService.service('DataResourceService.dataResource', ['$http',
+  function($http) {
+
+  }
+]);
