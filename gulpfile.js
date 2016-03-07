68d53721a14b4008331a84c069c5b6acdf101a59
uinika <uinika@163.com>
2016-03-07 18:01:18 +0800

    Update logo style in main.js

--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,7 @@ gulp.task('default', ['server', 'client']);
 gulp.task('client', ['less', 'connect', 'watch']);
 // Less Processor
 gulp.task('less', function() {
-  return gulp.src('./client/styles/lesses/' + 'index.less')
+  return gulp.src('./client/styles/less/' + 'index.less')
     .pipe(less())
     .pipe(gulp.dest('./client/styles/'));
 });
