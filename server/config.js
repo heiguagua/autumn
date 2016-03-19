9550b9774a3bdb647f017c56ac7ec0c55e463368
uinika <uinika@163.com>
2016-03-20 04:30:10 +0800

    Fix bug

--- server/config.js
+++ server/config.js
@@ -7,6 +7,16 @@ const Config = {
   mongodb: new Mongodb.Db('autumn', new Mongodb.Server('localhost', 27017), {safe: true}),
   json: function(name){
     return JsonLoader.sync(__dirname + '/mock/' + name);
+  },
+  head: function(body){
+    return {
+      "head": {
+        "status": 200,
+        "token": "bb4fba181e6d83d8c5a1b56055c08b5aa2da5755",
+        "message": "Mongodb Operation Sucess!"
+      },
+      "body": body
+    }
   }
 }
 
