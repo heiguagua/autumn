2e70761d748d87baecc8d1ead69af32886d4e184
uinika <uinika@163.com>
2016-03-20 02:40:13 +0800

    Connected Mongodb

--- server/api/resource-catalog.js
+++ server/api/resource-catalog.js
@@ -1,14 +1,21 @@
 'use strict';
 const Express = require('express'),
       Router = Express.Router(),
-      JsonLoader = require('load-json-file'),
-      Config = require('../config');
+      Config = require('../config'),
+      JsonLoader = require('load-json-file');
 
+// mongoimport --db autumn --collection resource_catalog --file server/mock/resource-catalog/get.json
 Router.route('/resource-catalog')
   .get(function(req, res) {
-    //GET for Read
-    let datas = JsonLoader.sync(Config.path + 'resource-catalog/get.json');
-    res.json(datas);
+    Config.mongodb.open(function(err, db) {
+      var ResourceCatalog = db.collection('resource_catalog').find({}, {_id: 0});
+      ResourceCatalog.each(function(err, doc) {
+        if (doc != null) {
+          res.json(doc);
+        }
+        db.close();
+      })
+    });
   })
   .post(function(req, res) {
     //POST for Create
--- server/config.js
+++ server/config.js
@@ -1,6 +1,13 @@
 'use strict';
+const Mongodb = require('mongodb'),
+      JsonLoader = require('load-json-file');
+
 const Config = {
-  path : __dirname + '/mock/'
+  path: __dirname + '/mock/',
+  mongodb: new Mongodb.Db('autumn', new Mongodb.Server('localhost', 27017), {safe: true}),
+  json: function(name){
+    return JsonLoader.sync(__dirname + '/mock/' + name);
+  }
 }
 
 module.exports = Config;
