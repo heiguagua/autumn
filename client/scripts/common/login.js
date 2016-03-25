faddcf4b4fef7487075d66d548908ee8e94b7f71
uinika <uinika@163.com>
2016-03-26 02:15:37 +0800

    Update jquery to 2.2.2

--- client/scripts/common/login.js
+++ client/scripts/common/login.js
@@ -3,24 +3,25 @@
 /*============ #Controller ============*/
 var LoginController = angular.module('LoginController', ['ui.router']);
 
-LoginController.controller('LoginController.login', ['$rootScope', '$scope', '$state', 'HttpAuth',
-  function($rootScope, $scope, $state, HttpAuth) {
+LoginController.controller('LoginController.login', ['$rootScope', '$scope', '$state',
+  function($rootScope, $scope, $state) {
     // Define a global object for current page
     $scope.login = {};
     // Binding submit event
     $scope.login.submit = function() {
-      var parameters = {
-        username: $scope.login.username,
-        password: $scope.login.password
-      }
-      HttpAuth.get({parameters},function(data){
-        if(data.head.status === 200 && data.head.token!=null){
-          var appScope = $rootScope;
-          appScope.token = data.head.token;
-          appScope.$broadcast('authChanged');
-          $state.go("main.dashboard");
-        }
-      });
+      // var parameters = {
+      //   username: $scope.login.username,
+      //   password: $scope.login.password
+      // }
+      // HttpAuth.get({parameters},function(data){
+      //   if(data.head.status === 200 && data.head.token!=null){
+      //     var appScope = $rootScope;
+      //     appScope.token = data.head.token;
+      //     appScope.$broadcast('authChanged');
+      //     $state.go("main.dashboard");
+      //   }
+      // });
+      $state.go("main.dashboard");
     }
   }
 ]);
