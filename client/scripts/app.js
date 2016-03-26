a992059c72e406f8f5e7db4393b64bfaca58dbe0
uinika <uinika@163.com>
2016-03-26 21:56:21 +0800

    Adjust code

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -4,12 +4,11 @@
 var app = angular.module('app', [
   'Config',
   'ui.router',
-  'restangular',
   'ui.bootstrap',
   'LoginController',
   'MainController',
   'DashboardController',
-  'ResourceCatalogModule',
+  'ResourceCatalog',
   'DataResourceController',
   'DataInfoController',
   'DataAuthorityController',
@@ -36,151 +35,151 @@ var app = angular.module('app', [
   'DataVisitStatisticController'
 ]);
 
-app.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', 'RestangularProvider', function($stateProvider, $urlRouterProvider, $locationProvider, RestangularProvider) {
-  $locationProvider.html5Mode(false);
-  $urlRouterProvider.otherwise('/login');
-  RestangularProvider.setBaseUrl('http://localhost:5000');
-  RestangularProvider.setDefaultHttpFields({withCredentials: true, cache: false});
-  $stateProvider
-    .state('login', {
-      url: '/login',
-      templateUrl: 'partials/common/login.html',
-      controller: 'LoginController.login'
-    })
-    .state('main', {
-      url: '/main',
-      templateUrl: 'partials/common/main.html',
-      controller: 'MainController.main'
-    })
-    .state('main.dashboard', {
-      url: '/dashboard',
-      templateUrl: 'partials/dashboard/dashboard.html',
-      controller: 'DashboardController.dashboard'
-    })
-    .state('main.resource-catalog', {
-      url: '/resource-catalog',
-      templateUrl: 'partials/data-resource/resource-catalog/resource-catalog.html',
-      controller: 'ResourceCatalogController.resourceCatalog'
-    })
-    .state('main.data-resource', {
-      url: '/data-resource',
-      templateUrl: 'partials/data-resource/metadata/data-resource.html',
-      controller: 'DataResourceController.dataResource'
-    })
-    .state('main.data-info', {
-      url: '/data-info',
-      templateUrl: 'partials/data-resource/metadata/data-info.html',
-      controller: 'DataInfoController.dataInfo'
-    })
-    .state('main.data-authority', {
-      url: '/data-authority',
-      templateUrl: 'partials/data-resource/metadata/data-authority.html',
-      controller: 'DataAuthorityController.dataAuthority'
-    })
-    .state('main.data-authority-apply', {
-      url: '/data-authority-apply',
-      templateUrl: 'partials/data-resource/metadata/data-authority-apply.html',
-      controller: 'DataAuthorityApplyController.dataAuthorityApply'
-    })
-    .state('main.data-audit-info', {
-      url: '/data-audit-info',
-      templateUrl: 'partials/data-resource/data/data-audit-info.html',
-      controller: 'DataAuditInfoController.dataAuditInfo'
-    })
-    .state('main.data-release-info', {
-      url: '/data-release-info',
-      templateUrl: 'partials/data-resource/data/data-release-info.html',
-      controller: 'DataReleaseInfoController.dataReleaseInfo'
-    })
-    .state('main.data-offline-info', {
-      url: '/data-offline-info',
-      templateUrl: 'partials/data-resource/data/data-offline-info.html',
-      controller: 'DataOfflineInfoController.dataOfflineInfo'
-    })
-    .state('main.data-visit-info', {
-      url: '/data-visit-info',
-      templateUrl: 'partials/data-status/data-visit-info.html',
-      controller: 'DataVisitInfoController.dataVisitInfo'
-    })
-    .state('main.plat-statistic', {
-      url: '/plat-statistic',
-      templateUrl: 'partials/data-status/plat-statistic.html',
-      controller: 'PlatStatisticController.platStatistic'
-    })
-    .state('main.plat-dept-statistic', {
-      url: '/plat-dept-statistic',
-      templateUrl: 'partials/data-status/plat-dept-statistic.html',
-      controller: 'PlatDeptStatisticController.platDeptStatistic'
-    })
-    .state('main.data-visit-statistic', {
-      url: '/data-visit-statistic',
-      templateUrl: 'partials/data-status/data-visit-statistic.html',
-      controller: 'DataVisitStatisticController.dataVisitStatistic'
-    })
-    .state('main.server-manage', {
-      url: '/server-manage',
-      templateUrl: 'partials/platform-operations/server-manage.html',
-      controller: 'ServerManageController.serverManage'
-    })
-    .state('main.contacts-group', {
-      url: '/contacts-group',
-      templateUrl: 'partials/platform-operations/platform-alarm/contacts-group.html',
-      controller: 'ContactsGroupController.contactsGroup'
-    })
-    .state('main.alarm-rule', {
-      url: '/alarm-rule',
-      templateUrl: 'partials/platform-operations/platform-alarm/alarm-rule.html',
-      controller: 'AlarmRuleController.alarmRule'
-    })
-    .state('main.alarm-info', {
-      url: '/alarm-info',
-      templateUrl: 'partials/platform-operations/platform-alarm/alarm-info.html',
-      controller: 'AlarmInfoController.alarmInfo'
-    })
-    .state('main.dcm-config', {
-      url: '/dcm-config',
-      templateUrl: 'partials/platform-configuration/dcm-config.html',
-      controller: 'DcmConfigController.dcmConfig'
-    })
-    .state('main.sys-setting-category', {
-      url: '/sys-setting-category',
-      templateUrl: 'partials/platform-configuration/sys-setting-category.html',
-      controller: 'SysSettingCategoryController.sysSettingCategory'
-    })
-    .state('main.sys-dict-category', {
-      url: '/sys-dict-category',
-      templateUrl: 'partials/platform-configuration/sys-dict-category.html',
-      controller: 'SysDictCategoryController.sysDictCategory'
-    })
-    .state('main.log-manage', {
-      url: '/log-manage',
-      templateUrl: 'partials/platform-configuration/log-manage.html',
-      controller: 'LogManageController.logManage'
-    })
-    .state('main.init-system', {
-      url: '/init-system',
-      templateUrl: 'partials/platform-configuration/init-system.html',
-      controller: 'InitSystemController.initSystem'
-    })
-    .state('main.sys-dep-manage', {
-      url: '/sys-dep-manage',
-      templateUrl: 'partials/organization-person/sys-dep-manage.html',
-      controller: 'SysDepManageController.sysDepManage'
-    })
-    .state('main.user-manage', {
-      url: '/user-manage',
-      templateUrl: 'partials/organization-person/user-manage.html',
-      controller: 'UserManageController.userManage'
-    })
-    .state('main.sys-role', {
-      url: '/sys-role',
-      templateUrl: 'partials/organization-person/sys-role.html',
-      controller: 'SysRoleController.sysRole'
-    })
-    .state('main.data-role', {
-      url: '/data-role',
-      templateUrl: 'partials/organization-person/data-role.html',
-      controller: 'DataRoleController.dataRole'
-    })
+app.config(['$stateProvider', '$urlRouterProvider', '$locationProvider',
+  function($stateProvider, $urlRouterProvider, $locationProvider) {
+    $locationProvider.html5Mode(false);
+    $urlRouterProvider.otherwise('/login');
+    $stateProvider
+      .state('login', {
+        url: '/login',
+        templateUrl: 'partials/common/login.html',
+        controller: 'LoginController.login'
+      })
+      .state('main', {
+        url: '/main',
+        templateUrl: 'partials/common/main.html',
+        controller: 'MainController.main'
+      })
+      .state('main.dashboard', {
+        url: '/dashboard',
+        templateUrl: 'partials/dashboard/dashboard.html',
+        controller: 'DashboardController.dashboard'
+      })
+      .state('main.resource-catalog', {
+        url: '/resource-catalog',
+        templateUrl: 'partials/data-resource/resource-catalog/resource-catalog.html',
+        controller: 'ResourceCatalog.Controller.Main'
+      })
+      .state('main.data-resource', {
+        url: '/data-resource',
+        templateUrl: 'partials/data-resource/metadata/data-resource.html',
+        controller: 'DataResourceController.dataResource'
+      })
+      .state('main.data-info', {
+        url: '/data-info',
+        templateUrl: 'partials/data-resource/metadata/data-info.html',
+        controller: 'DataInfoController.dataInfo'
+      })
+      .state('main.data-authority', {
+        url: '/data-authority',
+        templateUrl: 'partials/data-resource/metadata/data-authority.html',
+        controller: 'DataAuthorityController.dataAuthority'
+      })
+      .state('main.data-authority-apply', {
+        url: '/data-authority-apply',
+        templateUrl: 'partials/data-resource/metadata/data-authority-apply.html',
+        controller: 'DataAuthorityApplyController.dataAuthorityApply'
+      })
+      .state('main.data-audit-info', {
+        url: '/data-audit-info',
+        templateUrl: 'partials/data-resource/data/data-audit-info.html',
+        controller: 'DataAuditInfoController.dataAuditInfo'
+      })
+      .state('main.data-release-info', {
+        url: '/data-release-info',
+        templateUrl: 'partials/data-resource/data/data-release-info.html',
+        controller: 'DataReleaseInfoController.dataReleaseInfo'
+      })
+      .state('main.data-offline-info', {
+        url: '/data-offline-info',
+        templateUrl: 'partials/data-resource/data/data-offline-info.html',
+        controller: 'DataOfflineInfoController.dataOfflineInfo'
+      })
+      .state('main.data-visit-info', {
+        url: '/data-visit-info',
+        templateUrl: 'partials/data-status/data-visit-info.html',
+        controller: 'DataVisitInfoController.dataVisitInfo'
+      })
+      .state('main.plat-statistic', {
+        url: '/plat-statistic',
+        templateUrl: 'partials/data-status/plat-statistic.html',
+        controller: 'PlatStatisticController.platStatistic'
+      })
+      .state('main.plat-dept-statistic', {
+        url: '/plat-dept-statistic',
+        templateUrl: 'partials/data-status/plat-dept-statistic.html',
+        controller: 'PlatDeptStatisticController.platDeptStatistic'
+      })
+      .state('main.data-visit-statistic', {
+        url: '/data-visit-statistic',
+        templateUrl: 'partials/data-status/data-visit-statistic.html',
+        controller: 'DataVisitStatisticController.dataVisitStatistic'
+      })
+      .state('main.server-manage', {
+        url: '/server-manage',
+        templateUrl: 'partials/platform-operations/server-manage.html',
+        controller: 'ServerManageController.serverManage'
+      })
+      .state('main.contacts-group', {
+        url: '/contacts-group',
+        templateUrl: 'partials/platform-operations/platform-alarm/contacts-group.html',
+        controller: 'ContactsGroupController.contactsGroup'
+      })
+      .state('main.alarm-rule', {
+        url: '/alarm-rule',
+        templateUrl: 'partials/platform-operations/platform-alarm/alarm-rule.html',
+        controller: 'AlarmRuleController.alarmRule'
+      })
+      .state('main.alarm-info', {
+        url: '/alarm-info',
+        templateUrl: 'partials/platform-operations/platform-alarm/alarm-info.html',
+        controller: 'AlarmInfoController.alarmInfo'
+      })
+      .state('main.dcm-config', {
+        url: '/dcm-config',
+        templateUrl: 'partials/platform-configuration/dcm-config.html',
+        controller: 'DcmConfigController.dcmConfig'
+      })
+      .state('main.sys-setting-category', {
+        url: '/sys-setting-category',
+        templateUrl: 'partials/platform-configuration/sys-setting-category.html',
+        controller: 'SysSettingCategoryController.sysSettingCategory'
+      })
+      .state('main.sys-dict-category', {
+        url: '/sys-dict-category',
+        templateUrl: 'partials/platform-configuration/sys-dict-category.html',
+        controller: 'SysDictCategoryController.sysDictCategory'
+      })
+      .state('main.log-manage', {
+        url: '/log-manage',
+        templateUrl: 'partials/platform-configuration/log-manage.html',
+        controller: 'LogManageController.logManage'
+      })
+      .state('main.init-system', {
+        url: '/init-system',
+        templateUrl: 'partials/platform-configuration/init-system.html',
+        controller: 'InitSystemController.initSystem'
+      })
+      .state('main.sys-dep-manage', {
+        url: '/sys-dep-manage',
+        templateUrl: 'partials/organization-person/sys-dep-manage.html',
+        controller: 'SysDepManageController.sysDepManage'
+      })
+      .state('main.user-manage', {
+        url: '/user-manage',
+        templateUrl: 'partials/organization-person/user-manage.html',
+        controller: 'UserManageController.userManage'
+      })
+      .state('main.sys-role', {
+        url: '/sys-role',
+        templateUrl: 'partials/organization-person/sys-role.html',
+        controller: 'SysRoleController.sysRole'
+      })
+      .state('main.data-role', {
+        url: '/data-role',
+        templateUrl: 'partials/organization-person/data-role.html',
+        controller: 'DataRoleController.dataRole'
+      })
 
-}]);
+  }
+]);
--- client/scripts/data-resource/resource-catalog/resource-catalog.js
+++ client/scripts/data-resource/resource-catalog/resource-catalog.js
@@ -1,9 +1,9 @@
 'use strict';
-var ResourceCatalogModule = angular.module('ResourceCatalogModule', ['ui.router', 'GlobalModule']);
+var ResourceCatalog = angular.module('ResourceCatalog', ['ui.router', 'GlobalModule']);
 
 /** Main Controller */
-ResourceCatalogModule.controller('ResourceCatalogController.resourceCatalog', ['$scope', '$q', 'ResourceCatalogService.http', 'ResourceCatalogService.common',
-  function($scope, $q, http, common) {
+ResourceCatalog.controller('ResourceCatalog.Controller.Main', ['$scope', '$q', 'ResourceCatalog.Service.Http', 'ResourceCatalog.Service.Component',
+  function($scope, $q, Http, Component) {
     // Pagination parameter
     var paging = $scope.Paging = {};
     var pagingMaxSize = $scope.Paging.maxSize = 5;
@@ -17,26 +17,88 @@ ResourceCatalogModule.controller('ResourceCatalogController.resourceCatalog', ['
     // Modal object
     $scope.Modal = {};
 
-    /* Init */
-    (function(){
-      // Init Table when first enter current page
-      http.fatchResourceCatalog(_httpParams).then(function(data){
-        $scope.ResourceCatalogs = data.body;
-        $scope.Paging.totalItems = data.head.total;
+    /* Refresh */
+    $scope.Refresh = function(){
+      var _httpParams = {};
+      _httpParams.skip = 1;
+      _httpParams.limit = pagingItemsPerPage;
+      Http.fatchResourceCatalog(_httpParams).then(function(result) {
+        $scope.ResourceCatalogs = result.data.body;
+        $scope.Paging.totalItems = result.data.head.total;
+        $scope.Paging.currentPage = 1;
       });
-      // Handle the page changed event
+    }
+
+    /* Search */
+    $scope.Search = function(){
+      _httpParams.categoryName = $scope.CategoryName;
+      _httpParams.catalogName = $scope.CatalogName;
+      Http.fatchResourceCatalog(_httpParams).then(function(data){
+        $scope.ResourceCatalogs = data.body;
+      });
+    };
+
+    /* Create */
+    $scope.Create = function() {
+      $scope.Modal = {};
+      Component.popModal($scope, '添加', 'template-modal').result.then(function(_httpParams) {
+        // Save operation by promise
+        Http.saveResourceCatalog(_httpParams).then(function(result) {
+          if ('200' === result.data.head.status) {
+            return result.data.head;
+          }
+        }).then(function(head) {
+          $scope.Refresh();
+          head.type = 'success';
+          return head;
+        }).then(function(head){
+          Component.popAlert($scope, head);
+        }).then(function(){
+          checkedItemArray = []; // Empty checkedItemArray
+        })
+      });
+    };11
+
+    /* Update */
+    $scope.Update = function(){
+      if(1 === checkedItemArray.length){
+        Http.findResourceCatalogbyID(checkedItemArray[0]).then(function(result){
+          $scope.Modal = result.data.body;
+        }).then(function(){
+          return Component.popModal($scope, '修改', 'template-modal');
+        }).then(function(modalInstance){
+          modalInstance.result.then(function(_httpParams) {
+            Http.updateResourceCatalogbyID(_httpParams).then(function(result){
+              // TODO 更新成功之后，需要刷新表格的数据，并显示成功的提示消息
+              console.log(result);
+            })
+          });
+        })
+      }
+      else if(0 === checkedItemArray.length){
+        Component.popAlert($scope, {type:'warning', message:'请选择需要进行编辑的数据！'});
+      }
+      else{
+        Component.popAlert($scope, {type:'warning', message:'不能同时编辑多条数据！'});
+      }
+    };
+
+    /* Initialization Code Block*/
+    (function(){
+      // Handle Table
+      Http.fatchResourceCatalog(_httpParams).then(function(result){
+        $scope.ResourceCatalogs = result.data.body;
+        $scope.Paging.totalItems = result.data.head.total;
+      });
+      // Handle Pagination
       $scope.Paging.pageChanged = function() {
         _httpParams.skip = $scope.Paging.currentPage;
         _httpParams.limit = pagingItemsPerPage;
-        http.fatchResourceCatalog(_httpParams).then(function(data){
-          $scope.ResourceCatalogs = data.body;
+        Http.fatchResourceCatalog(_httpParams).then(function(result){
+          $scope.ResourceCatalogs = result.data.body;
         });
       };
-    })();
-
-    /* Checked Status */
-    (function(){
-      // Handle single checked
+      // Handle Single Checked
       $scope.CheckedChange = function(currentItem){
         if(currentItem.ResourceCatalog.CheckedStatus){
           if(-1 === _.indexOf(checkedItemArray, currentItem.ResourceCatalog.id)){
@@ -49,7 +111,7 @@ ResourceCatalogModule.controller('ResourceCatalogController.resourceCatalog', ['
         }
         console.log(checkedItemArray);
       };
-      // Handle multiple checked
+      // Handle Multiple Checked
       $scope.$watch('CheckedAll', function(newValue, oldValue){
         if(newValue){
           $scope.CheckedItemArray = [];
@@ -70,78 +132,11 @@ ResourceCatalogModule.controller('ResourceCatalogController.resourceCatalog', ['
       });
     })();
 
-    /* Refresh */
-    $scope.Refresh = function(){
-      var _httpParams = {};
-      _httpParams.skip = 1;
-      _httpParams.limit = pagingItemsPerPage;
-      http.fatchResourceCatalog(_httpParams).then(function(data) {
-        $scope.ResourceCatalogs = data.body;
-        $scope.Paging.totalItems = data.head.total;
-        $scope.Paging.currentPage = 1;
-      });
-    }
-
-    /* Search */
-    $scope.Search = function(){
-      _httpParams.categoryName = $scope.CategoryName;
-      _httpParams.catalogName = $scope.CatalogName;
-      http.fatchResourceCatalog(_httpParams).then(function(data){
-        $scope.ResourceCatalogs = data.body;
-      });
-    };
-
-    /* Create */
-    $scope.Create = function() {
-      $scope.Modal = {};
-      common.popModal($scope, '添加', 'template-modal').result.then(function(_httpParams) {
-        // Save operation by promise
-        http.saveResourceCatalog(_httpParams).then(function(data) {
-          if ('200' === data.head.status) {
-            return data.head;
-          }
-        }).then(function(head) {
-          $scope.Refresh();
-          head.type = 'success';
-          return head;
-        }).then(function(head){
-          common.popAlert($scope, head);
-        }).then(function(){
-          checkedItemArray = []; // Empty checkedItemArray
-        })
-      });
-    };11
-
-    /* Update */
-    $scope.Update = function(){
-      if(1 === checkedItemArray.length){
-        http.findResourceCatalogbyID(checkedItemArray[0]).then(function(data){
-          $scope.Modal = data.body;
-        }).then(function(){
-          return common.popModal($scope, '修改', 'template-modal');
-        }).then(function(modalInstance){
-          modalInstance.result.then(function(_httpParams) {
-            http.updateResourceCatalogbyID(_httpParams).then(function(data){
-              // TODO 更新成功之后，需要刷新表格的数据，并显示成功的提示消息
-              console.log(data);
-            })
-          });
-        })
-      }
-      else if(0 === checkedItemArray.length){
-        common.popAlert($scope, {type:'warning', message:'请选择需要进行编辑的数据！'});
-      }
-      else{
-        common.popAlert($scope, {type:'warning', message:'不能同时编辑多条数据！'});
-      }
-    };
-
-
   }
 ])
 
-/* Main Component */
-ResourceCatalogModule.service('ResourceCatalogService.common', ['$uibModal',
+/* Component */
+ResourceCatalog.service('ResourceCatalog.Service.Component', ['$uibModal',
   function($uibModal) {
     // prompt Alert
     function popAlert(scope, info) {
@@ -177,63 +172,28 @@ ResourceCatalogModule.service('ResourceCatalogService.common', ['$uibModal',
   }
 ])
 
-/* Main Service */
-ResourceCatalogModule.factory('ResourceCatalogService.http', ['$http', 'Restangular', '$q', 'API',
-  function($http, Restangular, $q, API) {
+/* HTTP */
+ResourceCatalog.factory('ResourceCatalog.Service.Http', ['$http', '$q', 'API',
+  function($http, $q, API) {
     function fetchResourceCatalog(params) {
-      var Qdefer = $q.defer();
-      var Qpromise = Qdefer.promise;
-      $http.get(
-        API.path + '/api/resource-catalog', {
-          cache: false,
-          params: params
-        }).success(function(data, status, headers, config) {
-        Qdefer.resolve(data);
-      }).error(function(data, status, headers, config) {
-        Qdefer.reject();
-      })
-      return Qpromise;
+      return $http.get(
+        API.path + '/api/resource-catalog', {params: params}
+      )
     };
-    function saveResourceCatalog(datas) {
-      var Qdefer = $q.defer();
-      var Qpromise = Qdefer.promise;
-      $http.post(
-        API.path + '/api/resource-catalog', {
-          cache: false,
-          data: datas
-        }).success(function(data, status, headers, config) {
-        Qdefer.resolve(data);
-      }).error(function(data, status, headers, config) {
-        Qdefer.reject();
-      })
-      return Qpromise;
+    function saveResourceCatalog(data) {
+      return $http.post(
+        API.path + '/api/resource-catalog', {data: data}
+      )
     };
     function findResourceCatalogbyID(id){
-      var Qdefer = $q.defer();
-      var Qpromise = Qdefer.promise;
-      $http.get(
-        API.path + '/api/resource-catalog/' + id, {
-          cache: false
-        }).success(function(data, status, headers, config) {
-        Qdefer.resolve(data);
-      }).error(function(data, status, headers, config) {
-        Qdefer.reject();
-      })
-      return Qpromise;
+      return $http.get(
+        API.path + '/api/resource-catalog/' + id
+      )
     };
     function updateResourceCatalogbyID(data){
-      var Qdefer = $q.defer();
-      var Qpromise = Qdefer.promise;
-      $http.put(
-        API.path + '/api/resource-catalog/' + data.id, {
-          data: data,
-          cache: false
-        }).success(function(data, status, headers, config) {
-        Qdefer.resolve(data);
-      }).error(function(data, status, headers, config) {
-        Qdefer.reject();
-      })
-      return Qpromise;
+      return $http.put(
+        API.path + '/api/resource-catalog/' + data.id, {data: data}
+      )
     };
     return {
       fatchResourceCatalog: fetchResourceCatalog,
