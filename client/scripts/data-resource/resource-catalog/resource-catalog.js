9dc977ba12dd8a1e3f4170f81079ba94d56cd901
uinika <uinika@163.com>
2016-03-28 02:51:54 +0800

    Support angular material

--- client/scripts/data-resource/resource-catalog/resource-catalog.js
+++ client/scripts/data-resource/resource-catalog/resource-catalog.js
@@ -4,25 +4,20 @@ var ResourceCatalog = angular.module('ResourceCatalog', ['ui.router', 'GlobalMod
 /** Main Controller */
 ResourceCatalog.controller('ResourceCatalog.Controller.Main', ['$scope', '$q', 'ResourceCatalog.Service.Http', 'ResourceCatalog.Service.Component',
   function($scope, $q, Http, Component) {
-    // Pagination parameter
-    var paging = $scope.Paging = {};
-    var pagingMaxSize = $scope.Paging.maxSize = 5;
-    var pagingItemsPerPage = $scope.Paging.itemsPerPage = 12;
-    // Init pagination parameters for Http
-    var _httpParams = {};
-    _httpParams.skip = 1;
-    _httpParams.limit = pagingItemsPerPage;
-    // Array for checked item's ID
-    var checkedItemArray = $scope.CheckedItemArray = [];
-    // Modal object
-    $scope.Modal = {};
+    // Config Pagination in View
+    $scope.Paging = {};
+    $scope.Paging.maxSize = 5;
+    $scope.Paging.itemsPerPage = 12;
+    // Init Pagination Parameters
+    var _pagination = {};
+    _pagination.skip = 1;
+    _pagination.limit = 12;
+    // Checked Item Array
+    var _checkedItemArray = $scope.checkedItemArray = [];
 
     /* Refresh */
     $scope.Refresh = function(){
-      var _httpParams = {};
-      _httpParams.skip = 1;
-      _httpParams.limit = pagingItemsPerPage;
-      Http.fatchResourceCatalog(_httpParams).then(function(result) {
+      Http.fatchResourceCatalog(_pagination).then(function(result) {
         $scope.ResourceCatalogs = result.data.body;
         $scope.Paging.totalItems = result.data.head.total;
         $scope.Paging.currentPage = 1;
@@ -31,16 +26,17 @@ ResourceCatalog.controller('ResourceCatalog.Controller.Main', ['$scope', '$q', '
 
     /* Search */
     $scope.Search = function(){
-      _httpParams.categoryName = $scope.CategoryName;
-      _httpParams.catalogName = $scope.CatalogName;
-      Http.fatchResourceCatalog(_httpParams).then(function(data){
+      httpParams = {};
+      httpParams.categoryName = $scope.CategoryName;
+      httpParams.catalogName = $scope.CatalogName;
+      Http.fatchResourceCatalog(_.assign(_pagination, httpParams)).then(function(data){
         $scope.ResourceCatalogs = data.body;
       });
     };
 
     /* Create */
     $scope.Create = function() {
-      $scope.Modal = {};
+      $scope.Modal = {}; // Clean scope of modal
       Component.popModal($scope, '添加', 'template-modal').result.then(function(_httpParams) {
         // Save operation by promise
         Http.saveResourceCatalog(_httpParams).then(function(result) {
@@ -54,15 +50,16 @@ ResourceCatalog.controller('ResourceCatalog.Controller.Main', ['$scope', '$q', '
         }).then(function(head){
           Component.popAlert($scope, head);
         }).then(function(){
-          checkedItemArray = []; // Empty checkedItemArray
+          _checkedItemArray = []; // Empty _checkedItemArray
         })
       });
     };
 
     /* Update */
     $scope.Update = function(){
-      if(1 === checkedItemArray.length){
-        Http.findResourceCatalogbyID(checkedItemArray[0]).then(function(result){
+      $scope.Modal = {}; // Clean scope of modal
+      if(1 === _checkedItemArray.length){
+        Http.findResourceCatalogbyID(_checkedItemArray[0]).then(function(result){
           $scope.Modal = result.data.body;
         }).then(function(){
           return Component.popModal($scope, '修改', 'template-modal');
@@ -79,12 +76,12 @@ ResourceCatalog.controller('ResourceCatalog.Controller.Main', ['$scope', '$q', '
             }).then(function(head){
               Component.popAlert($scope, head);
             }).then(function(){
-              checkedItemArray = []; // Empty checkedItemArray
+              _checkedItemArray = []; // Empty _checkedItemArray
             })
           });
         })
       }
-      else if(0 === checkedItemArray.length){
+      else if(0 === _checkedItemArray.length){
         Component.popAlert($scope, {type:'warning', message:'请选择需要进行编辑的数据！'});
       }
       else{
@@ -94,8 +91,8 @@ ResourceCatalog.controller('ResourceCatalog.Controller.Main', ['$scope', '$q', '
 
     /* Delete */
     $scope.Delete = function(){
-      if(checkedItemArray.length){
-        Http.deleteResourceCatalogByIDs(checkedItemArray).then(function(result){
+      if(_checkedItemArray.length){
+        Http.deleteResourceCatalogByIDs(_checkedItemArray).then(function(result){
           if ('200' === result.data.head.status) {
             return result.data.head;
           }
@@ -106,7 +103,7 @@ ResourceCatalog.controller('ResourceCatalog.Controller.Main', ['$scope', '$q', '
         }).then(function(head){
           Component.popAlert($scope, head);
         }).then(function(){
-          checkedItemArray = []; // Empty checkedItemArray
+          _checkedItemArray = []; // Empty _checkedItemArray
         })
       }
       else{
@@ -122,48 +119,46 @@ ResourceCatalog.controller('ResourceCatalog.Controller.Main', ['$scope', '$q', '
     /* Initialization Code Block*/
     (function(){
       // Handle Table
-      Http.fatchResourceCatalog(_httpParams).then(function(result){
+      Http.fatchResourceCatalog(_pagination).then(function(result){
         $scope.ResourceCatalogs = result.data.body;
         $scope.Paging.totalItems = result.data.head.total;
       });
       // Handle Pagination
       $scope.Paging.pageChanged = function() {
-        _httpParams.skip = $scope.Paging.currentPage;
-        _httpParams.limit = pagingItemsPerPage;
-        Http.fatchResourceCatalog(_httpParams).then(function(result){
+        Http.fatchResourceCatalog(_pagination).then(function(result){
           $scope.ResourceCatalogs = result.data.body;
         });
       };
       // Handle Single Checked
       $scope.CheckedChange = function(currentItem){
         if(currentItem.ResourceCatalog.CheckedStatus){
-          if(-1 === _.indexOf(checkedItemArray, currentItem.ResourceCatalog.id)){
-            checkedItemArray.push(currentItem.ResourceCatalog.id);
+          if(-1 === _.indexOf(_checkedItemArray, currentItem.ResourceCatalog.id)){
+            _checkedItemArray.push(currentItem.ResourceCatalog.id);
           };
         }else{
-          _.remove(checkedItemArray, function(checkedItem) {
+          _.remove(_checkedItemArray, function(checkedItem) {
             return checkedItem === currentItem.ResourceCatalog.id;
           });
         }
-        console.log(checkedItemArray);
+        console.log(_checkedItemArray);
       };
       // Handle Multiple Checked
       $scope.$watch('CheckedAll', function(newValue, oldValue){
         if(newValue){
-          $scope.CheckedItemArray = [];
+          _checkedItemArray = [];
           _($scope.ResourceCatalogs).each(function(value, key){
             value.CheckedStatus = true;
-            checkedItemArray.push(value.id);
+            _checkedItemArray.push(value.id);
           });
-          console.log(checkedItemArray);
+          console.log(_checkedItemArray);
         }
         else{
-          if (checkedItemArray.length == 0) return;
+          if (_checkedItemArray.length == 0) return;
           _($scope.ResourceCatalogs).each(function(value, key){
             value.CheckedStatus = false;
           });
-          checkedItemArray = [];
-          console.log(checkedItemArray);
+          _checkedItemArray = [];
+          console.log(_checkedItemArray);
         }
       });
     })();
